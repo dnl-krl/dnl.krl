a226a7245e5ad5701d578707fab86c53cfa3f058
dnl-krl <71923099+dnl-krl@users.noreply.github.com>
2020-10-14 22:34:37 +0300

    Update less7.py

--- less7.py
+++ less7.py
@@ -36,3 +36,25 @@ my_print()
 
 # 4.
 
+my_dict_1 = {'a': 1, 'b': 10, 'c': '10', 'd': 'hello', 'f': 16.09, 'e': 'world'}
+my_dict_2 = {'b': 10, 'd': 'hello', 'e': 'world', 'f': 16.09}
+
+dict_1 = set(my_dict_1)
+dict_2 = set(my_dict_2)
+
+my_res_sym = dict_1.intersection(my_dict_2)
+print(list(my_res_sym))
+
+my_res_dif = dict_1.difference(my_dict_2)
+print(list(my_res_dif))
+
+new_dict = dict()
+for key, value in my_dict_1.items() and my_dict_2.items():
+    if key not in my_dict_2:
+        new_dict.update(key, value)
+        print(new_dict)
+    else:
+        pass
+
+print(my_dict_1.keys())
+
