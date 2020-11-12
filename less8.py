c03ef1597ef50ed0a08a91b7767fb4808fba3ae1
dnl-krl <71923099+dnl-krl@users.noreply.github.com>
2020-11-12 11:50:32 +0200

    Create less8.py

---
+++ less8.py
@@ -0,0 +1,30 @@
+import random
+
+def names():
+    name = open('names.txt', 'r')
+    result = []
+    for line in name.readlines():
+        r_name = line.split()[1]
+        result.append(r_name)
+    print(result)
+
+result = names()
+
+#############################################
+
+def names():
+    name = open('domains.txt', 'r')
+    result = []
+    for line in name.readlines():
+        line = line.rstrip()
+        result.append(line[1::])
+    print(result)
+
+result = names()
+
+#############################################
+
+def gen_mail():
+    print(f'names()+)
+
+result = names()
